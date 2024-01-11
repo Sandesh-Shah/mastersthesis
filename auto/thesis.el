51fee3e10548b77ff95ab0c5b9e469994baca547
sandeshthakuri111@gmail.com <sandeshthakuri111@gmail.com>
2024-01-11 22:10:27 +0545

    Second

--- auto/thesis.el
+++ auto/thesis.el
@@ -3,12 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("memoir" "a4paper" "12pt" "oneside" "onecolumn" "openany" "final")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "premilinaries/symbolconvention"
     "chapters/chapter1"
     "chapters/chapter3"
     "chapters/chapter4"
--- auto/thesis_preamble.el
+++ auto/thesis_preamble.el
@@ -1,11 +1,13 @@
 (TeX-add-style-hook
  "thesis_preamble"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
-    "url"))
+    "url"
+    "amsmath"
+    "amssymb"))
  :latex)
 
