1124546414760685af63a43cc02ea5f57718bc16
sandeshthakuri111@gmail.com <sandeshthakuri111@gmail.com>
2024-01-11 12:57:10 +0545

    First push

---
+++ auto/thesis.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "thesis"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("memoir" "a4paper" "12pt" "oneside" "onecolumn" "openany" "final")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (TeX-run-style-hooks
+    "latex2e"
+    "chapters/chapter1"
+    "chapters/chapter3"
+    "chapters/chapter4"
+    "chapters/chapter5"
+    "memoir"
+    "memoir12"
+    "thesis_preamble"))
+ :latex)
+
