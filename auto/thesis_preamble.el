87c8e56686e2e9ac0c5d86ab6460680207b4d48e
sandeshthakuri111@gmail.com <sandeshthakuri111@gmail.com>
2024-01-13 13:56:33 +0545

    Third

--- auto/thesis_preamble.el
+++ auto/thesis_preamble.el
@@ -1,13 +1,28 @@
 (TeX-add-style-hook
  "thesis_preamble"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "url"
+    "graphicx"
+    "color"
+    "titling"
     "amsmath"
-    "amssymb"))
+    "amssymb"
+    "amsthm"
+    "mathtools")
+   (LaTeX-add-amsthm-newtheorems
+    "definition"
+    "example"
+    "exe"
+    "prob"
+    "task"
+    "theorem"
+    "corollary"
+    "lemma"
+    "remark"))
  :latex)
 
