e71919f7ac6107cffc39800970e91d38e2ca4478
sandeshthakuri111@gmail.com <sandeshthakuri111@gmail.com>
2024-03-13 13:17:17 +0545

    Like final

--- auto/thesis.el
+++ auto/thesis.el
@@ -6,20 +6,22 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("memoir" "a4paper" "12pt" "oneside" "onecolumn" "openany" "final")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "premilinaries/titlepage"
+    "premilinaries/copyright"
     "premilinaries/dedication"
     "premilinaries/studentsdecelaration"
     "premilinaries/recommendation"
     "premilinaries/approval"
     "premilinaries/acknow"
+    "premilinaries/abs"
     "premilinaries/symbolconvention"
     "chapters/chapter1"
     "chapters/chapter2"
@@ -29,6 +31,7 @@
     "chapters/chapter5"
     "chapters/coding"
     "chapters/crypto"
+    "chapters/summary"
     "memoir"
     "memoir12"
     "thesis_preamble")
